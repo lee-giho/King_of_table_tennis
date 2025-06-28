11feb815f80545053708e00d15a9ddc35ab3e50c
giho <ff6022@naver.com>
2025-06-28 11:36:05 +0900

    스프링부트 프로젝트 생성

---
+++ src/main/java/com/giho/king_of_table_tennis/KingOfTableTennisApplication.java
@@ -0,0 +1,13 @@
+package com.giho.king_of_table_tennis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KingOfTableTennisApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(KingOfTableTennisApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/giho/king_of_table_tennis/KingOfTableTennisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.giho.king_of_table_tennis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KingOfTableTennisApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
