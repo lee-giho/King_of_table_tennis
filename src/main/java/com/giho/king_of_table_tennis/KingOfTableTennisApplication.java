8d77f3840c4b893d306dff8d0ffe95cafae87fa7
giho <ff6022@naver.com>
2025-06-29 23:08:24 +0900

    gitignore 수정 / application 파일을 properties -> yml로 변경 / MySQL, Redis 연결

--- src/main/java/com/giho/king_of_table_tennis/KingOfTableTennisApplication.java
+++ src/main/java/com/giho/king_of_table_tennis/KingOfTableTennisApplication.java
@@ -1,5 +1,6 @@
 package com.giho.king_of_table_tennis;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class KingOfTableTennisApplication {
 
   public static void main(String[] args) {
+
+    // .env 파일 로드
+    Dotenv dotenv = Dotenv.configure().load();
+
+    System.setProperty("MYSQL_PASSWORD", dotenv.get("MYSQL_PASSWORD"));
+
     SpringApplication.run(KingOfTableTennisApplication.class, args);
   }
 
