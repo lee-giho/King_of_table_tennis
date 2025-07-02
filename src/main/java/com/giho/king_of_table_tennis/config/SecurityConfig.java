b75d21ee7aa74b5a56cf5350ea0bb7b7c042dde7
giho <ff6022@naver.com>
2025-07-02 16:23:29 +0900

    UsernamePasswordAuthenticationFilter를 custom한 loginFilter로 대체를 통해 로그인 구현 / 로그인 동작

--- src/main/java/com/giho/king_of_table_tennis/config/SecurityConfig.java
+++ src/main/java/com/giho/king_of_table_tennis/config/SecurityConfig.java
@@ -1,17 +1,30 @@
 package com.giho.king_of_table_tennis.config;
 
+import com.giho.king_of_table_tennis.jwt.LoginFilter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+  private final AuthenticationConfiguration authenticationConfiguration;
+
+  @Bean
+  public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+    return configuration.getAuthenticationManager();
+  }
 
   @Bean
   public BCryptPasswordEncoder bCryptPasswordEncoder() {
@@ -27,11 +40,13 @@ public class SecurityConfig {
       .httpBasic(AbstractHttpConfigurer::disable) // http basic 인증 방식 disable
       .authorizeHttpRequests((auth) -> auth // 경로별 인가 작업
         .requestMatchers(
-          "/api/auth/**"
+          "/api/auth/**",
+          "/login"
         ).permitAll()
         .requestMatchers("/admin").hasRole("ADMIN")
         .anyRequest().authenticated()
       )
+      .addFilterAt(new LoginFilter(authenticationManager(authenticationConfiguration)), UsernamePasswordAuthenticationFilter.class) // UsernamePasswordAuthenticationFilter를 custom한 LoginFilter()로 대체
       .sessionManagement((session) -> session // 세션 설정
         .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
       );
---
+++ src/main/java/com/giho/king_of_table_tennis/service/CustomUserDetailsService.java
@@ -0,0 +1,29 @@
+package com.giho.king_of_table_tennis.service;
+
+import com.giho.king_of_table_tennis.dto.CustomUserDetails;
+import com.giho.king_of_table_tennis.entity.UserEntity;
+import com.giho.king_of_table_tennis.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+  private final UserRepository userRepository;
+
+  @Override
+  public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+    UserEntity userEntity = userRepository.findById(username).orElse(null);
+
+    if (userEntity != null) {
+      return new CustomUserDetails(userEntity);
+    }
+
+    return null;
+  }
+}
